55ef42e306cd2a13435f970aafd7d3b84afced66
Shawn Toubeau <shawntoubeau@gmail.com>
2018-12-18 16:49:40 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+mongoose.connect("mongodb://shawntoubeau:password123@ds135724.mlab.com:35724/gql-shawntoubeau");
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log("Listening for requests on port 4000");
+    
+})
